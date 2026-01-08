2d10a6844773cbef00b1b747cfb844ebc33075a0
Unnati Pitla <unnatipitla1026@gmail.com>
2026-01-08 20:54:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+const hamburger = document.getElementById("hamburger");
+    const menu = document.getElementById("menu");
+    const links = document.querySelectorAll(".menu-link");
 
+    // Toggle menu open/close
+    hamburger.addEventListener("click", () => {
+      menu.classList.toggle("active");
+      document.body.classList.toggle("no-scroll");
+    });
+
+    // Close menu on link click
+    links.forEach(link => {
+      link.addEventListener("click", () => {
+        menu.classList.remove("active");
+        document.body.classList.remove("no-scroll");
+      });
+    });
