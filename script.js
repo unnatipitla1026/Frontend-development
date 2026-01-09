316ea7a8b5b4e7a62ee6a6b1d6dd60309fe6eaa7
Unnati Pitla <unnatipitla1026@gmail.com>
2026-01-09 16:33:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
- document.getElementById("myForm").addEventListener("submit", function (e) {
+/*hamburger code*/
+    const hamburger = document.getElementById("hamburger");
+    const menu = document.getElementById("menu");
+    const links = document.querySelectorAll(".menu-link");
+
+    // Toggle menu open/close
+    hamburger.addEventListener("click", () => {
+      menu.classList.toggle("active");
+      document.body.classList.toggle("no-scroll");
+    });
+
+    // Close menu on link click
+    links.forEach(link => {
+      link.addEventListener("click", () => {
+        menu.classList.remove("active");
+        document.body.classList.remove("no-scroll");
+      });
+    });
+
+/*day 5*/
+    document.getElementById("myForm").addEventListener("submit", function (e) {
       e.preventDefault();
 
       const input = document.getElementById("textInput");
