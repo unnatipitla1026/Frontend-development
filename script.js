39c60bec58e7a279d6b29a9c96634e1e1c2cd4b7
Unnati Pitla <unnatipitla1026@gmail.com>
2026-01-14 16:05:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,3 +30,39 @@ function toggleMenu(){
     function hideNotification() {
       notification.style.display = "none";
     }
+
+/* Form input & submit  */
+const usernameInput = document.getElementById("username");
+const errorMsg = document.getElementById("errorMsg");
+const submitBtn = document.getElementById("submitBtn");
+const contactForm = document.getElementById("contactForm");
+const successMsg = document.getElementById("successMsg");
+
+if (usernameInput && submitBtn && contactForm) {
+
+  // Enable / disable submit button
+  usernameInput.addEventListener("input", () => {
+    if (usernameInput.value.trim() === "") {
+      submitBtn.disabled = true;
+      errorMsg.style.display = "block";
+    } else {
+      submitBtn.disabled = false;
+      errorMsg.style.display = "none";
+    }
+  });
+
+  // Prevent empty submission
+  contactForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (usernameInput.value.trim() === "") {
+      errorMsg.style.display = "block";
+      submitBtn.disabled = true;
+      return;
+    }
+successMsg.textContent = "Form submitted successfully!";
+errorMsg.style.display = "none";
+usernameInput.value = "";
+submitBtn.disabled = true;
+  });
+}
